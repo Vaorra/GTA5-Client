8cfd1f25bdd231714ac12137f4f9c730c93ad841
Jovin Risch <jovris@intnet.ch>
2019-10-15 13:42:53 +0200

    update spec func name

--- clientside_packages/AdminTools/events.ts
+++ clientside_packages/AdminTools/events.ts
@@ -1,5 +1,5 @@
 
-mp.events.add("spectatedPlayer", (target) => {
+mp.events.add("spectatePlayer", (target) => {
     mp.gui.chat.push(JSON.stringify(target))
     let cam = mp.cameras.new("specCam") // example, cam gets created
     cam.attachTo(target.handle, 10.0, 0.0, 10.0, true); 
