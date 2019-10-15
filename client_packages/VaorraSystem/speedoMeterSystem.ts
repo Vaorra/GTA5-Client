b2f9d63cbc53807e8d89cbdb05d5e7317b45b08e
Jovin Risch <jovris@intnet.ch>
2019-10-15 22:12:35 +0200

    init SpeedometerSystem

---
+++ client_packages/VaorraSystem/speedoMeterSystem.ts
@@ -0,0 +1,7 @@
+function playerEnterVehicleHandler(vehicle, seat) {
+    mp.gui.chat.push(`You got into the car with ID: ${vehicle.id}. Seat: ${seat}`);
+ }
+ 
+ mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
+
+ 
