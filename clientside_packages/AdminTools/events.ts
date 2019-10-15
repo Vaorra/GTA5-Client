81ec301a765eb4e3ce1b0f5512e34623830fcac6
Jovin Risch <jovris@intnet.ch>
2019-10-15 13:40:29 +0200

    update spec target
    update spec target

--- clientside_packages/AdminTools/events.ts
+++ clientside_packages/AdminTools/events.ts
@@ -1,6 +1,6 @@
 
 mp.events.add("spectatedPlayer", (target) => {
-    mp.gui.chat.push(target.name)
+    mp.gui.chat.push(JSON.stringify(target))
     let cam = mp.cameras.new("specCam") // example, cam gets created
     cam.attachTo(target.handle, 10.0, 0.0, 10.0, true); 
     cam.setActive(true);
