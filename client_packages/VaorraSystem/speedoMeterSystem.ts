799e8071ffb15dea2d00dcf0657eab424bd591ff
Jovin Risch <jovris@intnet.ch>
2019-10-15 22:45:29 +0200

    update speedometer

--- client_packages/VaorraSystem/speedoMeterSystem.ts
+++ client_packages/VaorraSystem/speedoMeterSystem.ts
@@ -1,7 +1,18 @@
+let speedoMeterHTML = "package://VaorraSystem/speedometerUI/speedometerUI.html";
+let speedometerUI:any = null;
 function playerEnterVehicleHandler(vehicle:any, seat:any) {
-    mp.gui.chat.push(`You got into the car with ID: ${vehicle.id}. Seat: ${seat}`);
+    if (seat === 0) 
+        mp.gui.chat.push(`You got into the car, driver seat`);
+        speedometerUI = mp.browsers.new(speedoMeterHTML);
  }
  
  mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
 
- 
+ mp.events.add("playerLeaveVehicle", () => {
+    mp.gui.chat.push("Player start leave the vehicle");
+
+    mp.browsers.forEach(browser => {
+        if (browser.url === speedoMeterHTML)
+            browser.destroy();
+    });
+})
